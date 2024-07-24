2acab4662255073c95f4215c98444f9845b22d17
Tribhuwan Kumar <tribhuwankumar735275@gmail.com>
2024-07-25 00:00:36 +0530

    fix: padding in github sec

--- src/views/GithubView.vue
+++ src/views/GithubView.vue
@@ -11,11 +11,11 @@
             <div class="flex-grow border-t border-black/20 dark:border-white/50 border-1"></div>
         </div>
         <div v-if="data" class="pl-5 grid xl:grid-cols-2 gap-8" :class="[visible ? 'translate-y-0 opacity-1 blur-0' : 'translate-y-4 opacity-0 blur-sm', 'transition-all motion-reduce:transition-none duration-500']">
-            <div v-for="repo in data.viewer.pinnedItems.nodes" :key="repo.id" class="h-44 p-8 rounded-xl dark:border-white/10 border border-button-color/50 shadow-sm shadow-button-color/10" :class="[visible ? 'translate-y-0 opacity-1 blur-0' : 'translate-y-4 opacity-0 blur-sm', 'transition-all motion-reduce:transition-none duration-500']">
+            <div v-for="repo in data.viewer.pinnedItems.nodes" :key="repo.id" class="h-44 p-[20px] sm:p-8 rounded-xl dark:border-white/10 border border-button-color/50 shadow-sm shadow-button-color/10" :class="[visible ? 'translate-y-0 opacity-1 blur-0' : 'translate-y-4 opacity-0 blur-sm', 'transition-all motion-reduce:transition-none duration-500']">
                 <a :href="repo.url" target="_blank" class="underline pr-5 text-slate-1000 dark:text-slate-300" :class="[visible ? 'translate-y-0 opacity-1 blur-0' : 'translate-y-4 opacity-0 blur-sm', 'transition-all motion-reduce:transition-none duration-500']">{{ repo.name }}</a>
                 <font-awesome-icon icon="fa-regular fa-star" class="text-lg pr-1 text-slate-500 dark:text-slate-300"></font-awesome-icon>
                 <span class="text-slate-500 dark:text-slate-300" :class="[visible ? 'translate-y-0 opacity-1 blur-0' : 'translate-y-4 opacity-0 blur-sm', 'transition-all motion-reduce:transition-none duration-500']">{{ repo.stargazerCount }}</span>
-                <p class="text-base pt-3 text-slate-500 dark:text-slate-300" :class="[visible ? 'translate-y-0 opacity-1 blur-0' : 'translate-y-4 opacity-0 blur-sm', 'transition-all motion-reduce:transition-none duration-500']">{{ repo.description }}</p>
+                <p class="text-base pt-3 text-slate-500 dark:text-slate-300" :class="[visible ? 'translate-y-0 opacity-1 blur-0 text-balance' : 'translate-y-4 opacity-0 blur-sm', 'transition-all motion-reduce:transition-none duration-500']">{{ repo.description }}</p>
             </div>
         </div>
         <div v-if="error">Error in loading data: {{ error.message }}</div>
